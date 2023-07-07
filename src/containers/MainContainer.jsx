153230eb2b0a5dea07a8de7ec6cca3b30d0339c4
Kevin Fan <kevinlifan@yahoo.com>
2023-07-07 14:30:00 -0400

    fixed scrolling

--- src/containers/MainContainer.jsx
+++ src/containers/MainContainer.jsx
@@ -5,10 +5,11 @@ export default function main() {
   window.addEventListener(
     'scroll',
     () => {
-      document.body.style.setProperty(
-        '--scroll',
-        window.scrollY / (document.body.offsetHeight - window.innerHeight)
-      );
+      document.body.style.setProperty('--scroll', window.scrollY);
+      let bodyScroll = document.querySelector('body').style.cssText.slice(10);
+      bodyScroll = Number(bodyScroll.slice(0, bodyScroll.length - 1));
+      if (bodyScroll > 1900) document.querySelector('body').setAttribute('logo', 'stop');
+      else document.querySelector('body').setAttribute('logo', 'go');
     },
     false
   );
