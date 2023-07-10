4c866af211301023ba16ba8193f29b43331543d0
Kevin Fan <kevinlifan@yahoo.com>
2023-07-10 01:15:27 -0400

    scheduled by instructor

--- src/containers/ScheduleContainer.jsx
+++ src/containers/ScheduleContainer.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import styles from './ScheduleContainer.module.scss';
+import { Dropdown } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { setInstructor } from '../store/slices/appSlice';
 
 export default function ScheduleContainer() {
+  const instructor = useSelector((state) => state.appSlice.instructor);
+  const dispatch = useDispatch();
+
   // TODO turn this into a fetch request to a database instead of hard writing each item and use state
   return (
     <div className={styles.scheduleDiv}>
@@ -9,6 +15,20 @@ export default function ScheduleContainer() {
         <h1>Upcoming Sessions!</h1>
         <p>All times are in EST</p>
       </div>
+      <p className={styles.dropdown}>Filter by instructor: </p>{' '}
+      <Dropdown className={styles.dropdown}>
+        <Dropdown.Toggle variant='success' id='dropdown-basic'>
+          {instructor}
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item onClick={() => dispatch(setInstructor('Mahir'))}>Mahir</Dropdown.Item>
+          <Dropdown.Item onClick={() => dispatch(setInstructor('Jimmy'))}>Jimmy</Dropdown.Item>
+          <Dropdown.Item onClick={() => dispatch(setInstructor('Kevin'))}>Kevin</Dropdown.Item>
+          <Dropdown.Item onClick={() => dispatch(setInstructor('Joseph'))}>Joseph</Dropdown.Item>
+          <Dropdown.Item onClick={() => dispatch(setInstructor('Praise'))}>Praise</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
       <div className={styles.event}>
         <h3>Arrays and Strings w/ Joseph</h3>
         <p>Monday July 10, 2023 @ 1pm-3pm</p>
@@ -27,19 +47,19 @@ export default function ScheduleContainer() {
       </div>
       <div className={styles.event}>
         <h3>Sliding Window w/ Mahir</h3>
-        <p>July 17, 2023 @ 1pm-3pm</p>
+        <p>Monday July 17, 2023 @ 1pm-3pm</p>
       </div>
       <div className={styles.event}>
         <h3>Binary Trees w/ Praise</h3>
-        <p>July 18, 2023 @ 9pm-11pm</p>
+        <p>Tuesday July 18, 2023 @ 9pm-11pm</p>
       </div>
       <div className={styles.event}>
         <h3>Graphs w/ Kevin</h3>
-        <p>July 19, 2023 @ 1pm-3pm</p>
+        <p>Wednesday July 19, 2023 @ 1pm-3pm</p>
       </div>
       <div className={styles.event}>
         <h3>Linked Lists w/ Kevin</h3>
-        <p>July 20, 2023 @ 9pm-11pm</p>
+        <p>Thursday July 20, 2023 @ 9pm-11pm</p>
       </div>
     </div>
   );
--- src/store/slices/appSlice.js
+++ src/store/slices/appSlice.js
@@ -4,14 +4,18 @@ export const appSlice = createSlice({
   name: 'appSlice',
   initialState: {
     colorTheme: null,
+    instructor: 'All',
   },
   reducers: {
     setColorTheme: (state, action) => {
       state.colorTheme = action.payload;
     },
+    setInstructor: (state, action) => {
+      state.instructor = action.payload;
+    },
   },
 });
 
-export const { setColorTheme } = appSlice.actions;
+export const { setColorTheme, setInstructor } = appSlice.actions;
 
 export default appSlice.reducer;
