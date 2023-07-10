d7d1eec26ae0ffbae164cd73dd978d94a16aab93
Kevin Fan <kevinlifan@yahoo.com>
2023-07-10 00:34:55 -0400

    added introduction

--- src/containers/MainContainer.jsx
+++ src/containers/MainContainer.jsx
@@ -15,10 +15,27 @@ export default function main() {
   );
 
   return (
-    <div className={styles.bigDiv}>
-      <p className={styles.mainLogo}>eLEET</p>
-      <div className={styles.opaqueDiv}></div>
-      <p>Join our community of </p>
-    </div>
+    <>
+      <div className={styles.bigDiv}>
+        <p className={styles.mainLogo}>eLEET</p>
+        <div className={styles.opaqueDiv}></div>
+      </div>
+      <div className={styles.intro}>
+        <h2>
+          Created by developers for developers. A community for <span className={styles.everyone}>everyone</span> who{' '}
+          <span className={styles.heart} onClick={(e) => (e.target.style.color = 'red')}>
+            ♥
+          </span>{' '}
+          code. <span className={styles.learn}>Learn</span> together, <span className={styles.grow}>grow</span>{' '}
+          together.
+        </h2>
+        <p className={styles.introText}>
+          Whether you've been an engineering for 20 years or just started yesterday, I'm sure we have something for you.
+          Check out our schedule for our Algorithm white-boarding sessions. We have a hangout room Monday-Friday
+          10am-10pm for accountability, motivation, application sprints, question and answers, and more! Join our
+          discord to meet like-minded engineers to hangout and stay up to date with any upcoming events!
+        </p>
+      </div>
+    </>
   );
 }
