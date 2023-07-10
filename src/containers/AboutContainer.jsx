050e5fda1ab96d8d61778044f5327ed0297f90c1
Kevin Fan <kevinlifan@yahoo.com>
2023-07-09 22:24:48 -0400

    added about page

--- src/containers/AboutContainer.jsx
+++ src/containers/AboutContainer.jsx
@@ -6,6 +6,28 @@ export default function AboutContainer() {
     <div className={styles.aboutDiv}>
       <div className={styles.aboutHeader}>We Love Algorithms</div>
       <div className={styles.founders}>Our Founders</div>
+      <div className={styles.bio}>
+        <div>
+          <p className={styles.names}>Jimmy Tran</p>
+          <p>Co-Founder and CEO</p>
+        </div>
+        <div>
+          <p className={styles.names}>Joseph Caballero</p>
+          <p>Co-Founder and CFO</p>
+        </div>
+        <div>
+          <p className={styles.names}>Praise Emmanuel</p>
+          <p>Co-Founder and COO</p>
+        </div>
+        <div>
+          <p className={styles.names}>Mahir Mohtasin</p>
+          <p>Co-Founder and CSO</p>
+        </div>
+        <div>
+          <p className={styles.names}>Kevin Fan</p>
+          <p>Co-Founder and CTO</p>
+        </div>
+      </div>
     </div>
   );
 }
